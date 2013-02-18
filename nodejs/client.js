c9276f2eb49d70e7fa126315cd6b8f6394143b79
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-18 17:44:53 +0100

    added client.js

---
+++ nodejs/client.js
@@ -0,0 +1,23 @@
+var app = require('http').createServer()
+  , io = require('socket.io').listen(app)
+
+  var socket = io.connect('http://54.246.80.107:8080');
+  
+  function broadcastMessage()
+  {
+    console.log('Broadcasting message: ' + $('#textmessage').val());
+	socket.emit('client',{message: $('#textmessage').val()});
+	return false;
+  }
+  //Log first message for debugging purposes
+  socket.on('news', function (data) {
+    console.log(data);
+  });
+  // on every message recived we print the new datas inside the #container div
+  socket.on('notification', function (data) {
+    console.log ("updating labels...");
+    console.log('Last Message: ' + data.message);
+    console.log('Last Update: ' + data.time);
+});
+
+broadcastMessage("you madafacka");
