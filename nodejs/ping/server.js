787316cdfbcbd42c04b0792597d289f6ae533615
Diego Lafuente <tufosa@gmail.com>
2013-02-24 14:04:00 +0100

    first version of the web client

--- nodejs/ping/server.js
+++ nodejs/ping/server.js
@@ -115,7 +115,7 @@ function serve_file(status, file, response)
 	fs.readFile('./html/' + file, function(err, data) {
 		if (err)
 		{
-			info ("error reading file: " + file + " - err: " + err);
+			info ("error reading file: " + file + " - err: " + err + ". Current dir: " + __dirname);
 			response.writeHead(404, {
 				'Content-Type': 'text/plain'
 			});
