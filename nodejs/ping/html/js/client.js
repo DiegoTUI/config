e495bcde0c6ed4a8bd32d2edcc33d619c2744579
Diego Lafuente <tufosa@gmail.com>
2013-02-24 18:55:09 +0100

    fixing index.html

--- nodejs/ping/html/js/client.js
+++ nodejs/ping/html/js/client.js
@@ -6,23 +6,32 @@
  var pingerClient = new function(){
  	// self-reference
 	var self = this;
-	//number of pingers to create
-	var numberOfPingers = 100;
 	//number of active pingers
 	var activePingers = 0;
+	//number of pingers to create
+	self.numberOfPingers = 100;
 	//period for each pinger
-	var period = 100;
+	self.period = 100;
 	//delay between the creation of each pinger
-	var delay = 1000;
+	self.delay = 1000;
+
+	/**
+ 	 * Set Params
+ 	 */
+ 	 self.setParams = function(numberofpingers, period, delay){
+ 	 	self.numberOfPingers = numberofpingers;
+ 	 	self.period = period;
+ 	 	self.delay = delay;
+ 	 }
 
 	/**
  	 * Create pingers
  	 */
  	self.createPingers = function(){
- 		$('#message').html('<p>Trying to create ' + numberOfPingers + ' pingers with a period of ' + period + 'ms and a delay between pingers of ' + delay +  'ms</p>');
- 	 	for (var i=0; i<numberOfPingers; i++)
+ 		$('#message').html('<p>Trying to create ' + self.numberOfPingers + ' pingers with a period of ' + self.period + 'ms and a delay between pingers of ' + self.delay +  'ms</p>');
+ 	 	for (var i=0; i<self.numberOfPingers; i++)
 		{
-			setTimeout(createPinger, delay * i);
+			setTimeout(createPinger, self.delay * i);
 		}
  	 }
 
@@ -33,7 +42,7 @@
 	{
 		info("Creating new pinger");
 		var p = new pinger();
-		p.start(period);
+		p.start(self.period);
 		activePingers++;
 		$('#activePingers').html(activePingers);
 	}
