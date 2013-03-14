4c7cb62b6ea0f78b07a0d9cd0e02cf6a3f28924c
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-14 13:49:58 +0100

    setloc script

---
+++ data/setloc_hotels.js
@@ -0,0 +1,6 @@
+
+db.hotels.find({"location.latitude":{$ne:null},
+				{"location.latitude":{$ne:null}}).forEach(function(hotel){
+	hotel.loc = [hotel.location.longitude, hotel.location.latitude];
+	db.hotels.save(hotel);
+});
