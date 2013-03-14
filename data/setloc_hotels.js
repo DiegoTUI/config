05c868e4383a2422822a71c0188b80e8f549aa1b
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-14 14:06:53 +0100

    floats instead of strings

--- data/setloc_hotels.js
+++ data/setloc_hotels.js
@@ -1,5 +1,5 @@
 
 db.hotels.find({"location.latitude":{$ne:null},"location.latitude":{$ne:null}}).forEach(function(hotel){
-	hotel.loc = [hotel.location.longitude, hotel.location.latitude];
+	hotel.loc = [1*hotel.location.longitude, 1*hotel.location.latitude];
 	db.hotels.save(hotel);
 });
