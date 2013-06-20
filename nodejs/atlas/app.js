312ba1d3ab814adea6a55f51e7948bc1e9230449
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-20 09:39:10 +0200

    added config.json

--- nodejs/atlas/app.js
+++ nodejs/atlas/app.js
@@ -17,6 +17,7 @@ process.title = 'atlasProxy';
  * Globals.
  */
 var config = JSON.parse(fs.readFileSync("config.json"));
+var atlas = require("atlas.js");
 //The app
 var app = express();
 //Serve root GET calls
@@ -31,7 +32,7 @@ console.log("listening on port: " + config.port);
 function serve (request, response) {
 	console.log("Serving for " + request.params.service + " with query: " + JSON.stringify(request.query));
 	response.setHeader("Content-Type", "text/plain");
-	response.send("Response: " + JSON.stringify(request.query));
+	response.send("Response: " + JSON.stringify(request.query) + ". Url: " + atlas.url);
   /*var url = urlParser.parse(request.url, true);
   info ("url: " + JSON.stringify(url));
   info ("url.pathname: " + url.pathname);
