ea36a8a6b0652bfa9cf6ff892db33cc060e470e7
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-20 11:36:28 +0200

    included first nodeunit test

--- nodejs/atlas/app.js
+++ nodejs/atlas/app.js
@@ -3,9 +3,7 @@
 /**
  * Requirements.
  */
-var http = require('http');
 var fs = require('fs');
-var url = require('url');
 var express = require('express');
 var httpRequest = require('request');
 
@@ -42,72 +40,4 @@ function serve (request, response) {
 			response.send("Response: " + body);
 		}
 	});
-	
-  /*var url = urlParser.parse(request.url, true);
-  info ("url: " + JSON.stringify(url));
-  info ("url.pathname: " + url.pathname);
-	if (url.pathname == '/')
-	{
-		serve_home(request, response);
-		return;
-	}
-	// avoid going out of the home dir
-	if (url.pathname.contains('..'))
-	{
-		serve_file(404, 'not_found.html', response);
-		return;
-	}
-	if (url.pathname.startsWith('/src/'))
-	{
-		serve_file(200, '..' + url.pathname, response);
-		return;
-	}
-	serve_file(200, url.pathname, response);*/
 }
-
-/**
- * Serve the home page.
- */
-/*function serve_home(request, response)
-{
-	console.log("serving home");
-	serve_file(200, 'index.html', response);
-}*/
-
-/*
- * Serve a file.
- */
-/*function serve_file(status, file, response)
-{
-	console.log ("serving file: " + file);
-	fs.readFile("html/" + file, function(err, data) {
-		if (err)
-		{
-			info ("error reading file: " + file + " - err: " + err + ". Current dir: " + __dirname);
-			response.writeHead(404, {
-				'Content-Type': 'text/plain'
-			});
-			response.end('Page not found');
-			return;
-		}
-		var type = 'text/html';
-		if (file.endsWith('.js'))
-		{
-			type = 'text/javascript';
-		}
-		if (file.endsWith('.css'))
-		{
-			type = 'text/css';
-		}
-		response.writeHead(status, {
-			'Content-Length': data.length,
-			'Content-Type': type
-		});
-		response.end(data);
-	});
-}*/
-
-
-	
-
-
