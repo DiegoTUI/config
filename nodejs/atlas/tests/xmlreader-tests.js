2605b8fc6a3cbfced8e03389db9e854600ed64e6
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-20 15:17:41 +0200

    minimized xmltests

--- nodejs/atlas/tests/xmlreader-tests.js
+++ nodejs/atlas/tests/xmlreader-tests.js
@@ -7,6 +7,17 @@
 
 var XmlReader = require("../XmlReader.js");
 
+var testXml = '<root> \
+<item> \
+<value>1</value> \
+</item> \
+<item> \
+<value>1</value> \
+</item> \
+</root>';
+
+var testDescriptionMap = ["value"];
+
 var ticketAvailString = '<TicketAvailRS xsi-schemaLocation="http://www.hotelbeds.com/schemas/2005/06/messages TicketAvailRS.xsd" totalItems="27" echoToken="DummyEchoToken"> \
 	<AuditData> \
 		<ProcessTime>647</ProcessTime> \
@@ -145,7 +156,41 @@ var ticketClassificationListMapAlt = [
 {'Classification':[{'Code':'@code'},
 					{'Name':''}]}];
 
-exports.ticketAvail = function (test) {
+exports.testXml = function (test) {
+	var xmlReader = new XmlReader (testXml, testDescriptionMap, 'item');
+	var parsedXml = xmlReader.readObjects();
+	console.log("parsedXml: " + JSON.stringify(parsedXml));
+	/*//Now chek some stuff about the parsed xml
+	test.ok(parsedXml instanceof Array, 'parsedXml is an array');
+	test.ok(parsedXml.length === 2, 'parsedXml has 2 elements');
+	test.ok(parsedXml[0].DateFrom === 'DateFrom1', 'dateFrom is correct in 1');
+	test.ok(parsedXml[0].DateTo === 'DateTo1', 'dateTo is correct in 1');
+	test.ok(parsedXml[1].DateFrom === 'DateFrom2', 'dateFrom is correct in 2');
+	test.ok(parsedXml[1].DateTo === 'DateTo2', 'dateTo is correct in 2');
+	test.ok(parsedXml[0].Currency === 'Euro1', 'Currency is correct in 1');
+	test.ok(parsedXml[0].CurrencyCode === 'EUR1', 'CurrencyCode is correct in 1');
+	test.ok(parsedXml[1].Currency === 'Euro2', 'Currency is correct in 2');
+	test.ok(parsedXml[1].CurrencyCode === 'EUR2', 'CurrencyCode is correct in 2');
+	test.ok(parsedXml[0].Name === 'Ticket1', 'Ticket name is correct in 1');
+	test.ok(parsedXml[1].Name === 'Ticket2', 'Ticket name is correct in 2');
+	for (var i=0; i<parsedXml.length; i++) {
+		var ImageList = parsedXml[i]['TicketInfo.ImageList.Image'.listify()];
+		var DescriptionList = parsedXml[i]['TicketInfo.DescriptionList.Description'.listify()];
+		test.ok(ImageList.length === 3, 'Only 3 images in the list: ' + ImageList.length);
+		for (var j=0; j<3; j++) {
+			test.ok(ImageList[j].Type === "S", "wrong image type in ticket " + i + " image " + j);
+			test.ok(ImageList[j].Url === "Image"+(i+1)+""+(j+1), "wrong image url in ticket " + i + " image " + j);
+		}
+		test.ok(DescriptionList.length === 2, 'Only 2 descriptions in the list: ' + DescriptionList.length);
+		for (var j=0; j<2; j++) {
+			test.ok(DescriptionList[j].Type === "generalDescription", "wrong description type in ticket " + i + " description " + j);
+			test.ok(DescriptionList[j].Description === "Description "+(i+1)+""+(j+1), "wrong description text in ticket " + i + " description " + j);
+		}
+	}*/
+    test.done();
+}
+
+/*exports.ticketAvail = function (test) {
 	var xmlReader = new XmlReader (ticketAvailString, ticketAvailMap, 'ServiceTicket');
 	var parsedXml = xmlReader.readObjects();
 	//Now chek some stuff about the parsed xml
@@ -178,7 +223,7 @@ exports.ticketAvail = function (test) {
     test.done();
 }
 
-/*exports.ticketAvailAlt = function (test) {
+exports.ticketAvailAlt = function (test) {
 	var xmlReader = new XmlReader (ticketAvailString, ticketAvailMapAlt);
 	var parsedXml = xmlReader.readObjects();
 	//Now chek some stuff about the parsed xml
