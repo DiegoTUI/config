3d8fa68feafe59596ce1d8759d6586adc6df582a
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-24 13:21:14 +0200

    TicketAvailRequest working

--- nodejs/atlas/run-tests.sh
+++ nodejs/atlas/run-tests.sh
@@ -1,3 +1,4 @@
 ../node_modules/nodeunit/bin/nodeunit ./tests/param-string-tests.js
 ../node_modules/nodeunit/bin/nodeunit ./tests/util-tests.js
 ../node_modules/nodeunit/bin/nodeunit ./tests/xmlreader-tests.js
+../node_modules/nodeunit/bin/nodeunit ./tests/ticket-avail-tests.js
