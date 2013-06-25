25d6be5d1b6df91738dda1e8c657bb841ff52055
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-25 16:31:13 +0200

    added ajax-tests to test offline feature

---
+++ nodejs/atlas/tests/ajax-tests.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * TuiInnovation
+ * Ajax unit tests
+ *
+ * Copyright (C) 2013 TuiInnovation.
+ */
+
+
+/* run tests */
+exports.offlineTest = function (test) {
+	test.expect(1);
+	function ok(result) {
+		test.ok(false, "OK should never be called with a non-existing url");
+		test.done();
+	}
+
+	function nok(error, statusCode)
+	{
+		console.log("error: " + JSON.stringify(error));
+		test.ok(true, "kk");
+		test.done();
+	}
+
+	var ajax = require('../ajax.js');
+	ajax.send({},'http://212.170.239.71/appservices/http/FrontendService', ok, nok);
+}
+
