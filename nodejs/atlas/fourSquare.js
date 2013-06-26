7ddf8e6b00f0606ca3877f991e61ff6d1e8018b2
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-26 10:37:40 +0200

    added FSVenueSearch

--- nodejs/atlas/fourSquare.js
+++ nodejs/atlas/fourSquare.js
@@ -9,11 +9,11 @@
 /**
  * Pseudo-global to store Atlas requests and description maps
  */
+ var mainUrl = 'https://api.foursquare.com/v2/';
  var fourSquare = {
   clientId: 'X3K5ZBVTWBXWUL0N0MJE0LMAWQMSMUGGEUS1MH0QS5O2BIUR',
   clientSecret: 'GJZS1WPNU5K5QMJOTA01MK2TRSDQBDM3ZBRMC40YFZ1BMZZG',
-  url: 'https://api.foursquare.com/v2/',
-	venueSearchUrl: this.url + 'venues/search'
+	venueSearchUrl: mainUrl + 'venues/search'
 };
 
 //export module
