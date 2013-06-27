ecd20bcc879519b49d0f7e52e8e821727e431941
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-27 16:31:33 +0200

    fixing ticketavailtest

--- nodejs/atlas/TicketAvailRequest.js
+++ nodejs/atlas/TicketAvailRequest.js
@@ -75,7 +75,7 @@ var TicketAvailRequest = function(queryParameters, descriptionMap, tag)
 			result = parsedResponse;
 		});
 		while (result === null){}
-		log.info("about to return parsed response: " + JSON.stringify(result));
+		log.info("about to return parsed response"); //+ JSON.stringify(result));
 		return result;
 	}
 
