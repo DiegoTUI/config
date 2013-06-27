eb3b9a6f5243672ae8a3bcec7352413f935f5a86
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-06-27 16:07:46 +0200

    completed fsvenuesearch tests. Fine tuned FSVenueSearch

--- nodejs/atlas/FSVenueSearch.js
+++ nodejs/atlas/FSVenueSearch.js
@@ -42,7 +42,8 @@ var FSVenueSearch = function(queryParameters)
 	 * data: the json response received
 	 */
 	function parseResponse(data) {
-		data = "error" in data ? JSON.parse(data.error) : JSON.parse(data);
+		//data = "error" in data ? JSON.parse(data.error) : JSON.parse(data);
+		data = JSON.parse(data);
 		if (("meta" in data)&&("code" in data["meta"])&&(data.meta.code != 200)) { //there is an error
 			return data.meta;
 		}
