85bb4a4e6354a132a342f550718d81bc4b9ddead
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-07-01 14:21:32 +0200

    removed html folder. Moved run-tests.sh

--- nodejs/atlas/run-tests.sh
+++
@@ -1,6 +0,0 @@
-../node_modules/nodeunit/bin/nodeunit ./tests/param-string-tests.js
-../node_modules/nodeunit/bin/nodeunit ./tests/util-tests.js
-../node_modules/nodeunit/bin/nodeunit ./tests/xmlreader-tests.js
-../node_modules/nodeunit/bin/nodeunit ./tests/ticket-avail-tests.js
-../node_modules/nodeunit/bin/nodeunit ./tests/ajax-tests.js
-../node_modules/nodeunit/bin/nodeunit ./tests/fs-venue-search-tests.js
