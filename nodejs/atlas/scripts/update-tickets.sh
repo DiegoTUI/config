9a20c06afc76ca0557a2219c20845125eb200854
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-07-04 16:10:09 +0200

    full paths

--- nodejs/atlas/scripts/update-tickets.sh
+++ nodejs/atlas/scripts/update-tickets.sh
@@ -1,2 +1,2 @@
-mongo tuiinnovation remove-tickets.js >> update-tickets.log
-node update-tickets.js >> update-tickets.log
+mongo tuiinnovation /etc/tuiinnovation/config/nodejs/atlas/script/remove-tickets.js >> /var/tuiinnovation/log/update-tickets.log
+node /etc/tuiinnovation/config/nodejs/atlas/script/update-tickets.js >> /var/tuiinnovation/log/update-tickets.log
