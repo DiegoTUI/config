1e2bad8924c8abdef6efb2c40b93b90506a9a538
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-07-29 12:31:20 +0200

    deleted remove.js

--- nodejs/atlas/scripts/update-tickets.sh
+++ nodejs/atlas/scripts/update-tickets.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-# mongo tuiinnovation /etc/tuiinnovation/config/nodejs/atlas/scripts/remove-tickets.js
 /usr/local/bin/node /etc/tuiinnovation/config/nodejs/atlas/scripts/update-tickets.js
