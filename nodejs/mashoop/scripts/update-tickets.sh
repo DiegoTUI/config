37f8ee2b879c91fc93803ab28ba5e819b8d2266c
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-06 12:30:27 +0200

    deleted nodejs

--- nodejs/mashoop/scripts/update-tickets.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-/usr/local/bin/node /etc/tuiinnovation/config/nodejs/mashoop/scripts/update-tickets.js
--- nodejs/start-mashoop.sh
+++
@@ -1,1 +0,0 @@
-forever start -a -l /var/tuiinnovation/log/node.log /etc/tuiinnovation/config/nodejs/mashoop/app.js
