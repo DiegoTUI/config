5cee963625d968a44088a2b3c72f6f0fcaeee84d
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-08-29 09:32:08 +0200

    changed email account

--- deploy/app.js
+++ deploy/app.js
@@ -143,8 +143,8 @@ function EmailLog() {
 	var response = new WebPageResponse();
 	var webPage = new WebPageLog(response);
 	var recipient = 'alejandrofer@gmail.com,dlafuente@tuitravel-ad.com';
-	var sender = 'alexfernandeznpm@gmail.com';
-	var password = 'rsoWkVtbTn2U4Q';
+	var sender = 'innovation.lab.palma@gmail.com';
+	var password = 'chickenbus';
 	var host = 'smtp.gmail.com';
 
 	// init
