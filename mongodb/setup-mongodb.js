24b6a2944f9711a30a75e190b9669be960c39dff
Diego Lafuente <dlafuente@tuitravel-ad.com>
2014-01-20 18:27:18 +0100

    included ensureIndex for loc

--- mongodb/setup-mongodb.js
+++ mongodb/setup-mongodb.js
@@ -6,4 +6,5 @@ db.apikeys.ensureIndex({key: 1}, {unique: true});
 db.tickets.ensureIndex({id: 1}, {unique: true});
 db.tickets.ensureIndex({code: 1}, {unique: false});
 db.tickets.ensureIndex({destinationCode: 1}, {unique: false});
+db.tickets.ensureIndex({loc: "2d"});
 
