986b96451b490195ca4db469b642aac8edd02661
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-01-21 08:56:45 +0100

    changed lod to 2dsphere

--- mongodb/setup-mongodb.js
+++ mongodb/setup-mongodb.js
@@ -6,5 +6,5 @@ db.apikeys.ensureIndex({key: 1}, {unique: true});
 db.tickets.ensureIndex({id: 1}, {unique: true});
 db.tickets.ensureIndex({code: 1}, {unique: false});
 db.tickets.ensureIndex({destinationCode: 1}, {unique: false});
-db.tickets.ensureIndex({loc: "2d"});
+db.tickets.ensureIndex({loc: "2dsphere"});
 
